b9e1ab3918e3ce426f313f72351d587f9dfdb605
Jerdan1980 <jeremytinana@gmail.com>
2020-05-18 14:38:47 -0400

    get bootstrap working

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Main from './Main';
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"></link>
-      <Main />
+			{//<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"></link>
+}
+			<Main />
     </div>
   );
 }
