3abf0024e9261fc6fdd7d0c7f672dac873e9ab4c
Jerdan1980 <jeremytinana@gmail.com>
2020-07-29 13:51:56 -0400

    Remove localhost

--- client/src/components/NAPTable.js
+++ client/src/components/NAPTable.js
@@ -46,7 +46,7 @@ const ListFAQs = (props) => {
 	}
 
 	useEffect(() => {
-		axios.get(`http://localhost:3001/api/nap/get_nap`) 
+		axios.get(`/api/nap/get_nap`) 
 			.then(res => {setNAP(res.data)});
 	}, []);
 
