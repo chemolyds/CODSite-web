efac3f9b9c651e0752bcb2857c9681a31de1af3e
Jerdan1980 <jeremytinana@gmail.com>
2020-08-10 10:41:45 -0400

    Added hidden modifier to pages

--- server/models/pageModel.js
+++ server/models/pageModel.js
@@ -4,6 +4,7 @@ const PageSchema = mongoose.Schema({
 	page: { type: String, required: true},
 	header: {type: String, required: true, default: ""},
 	contents: { type: String, required: true},
+	hidden: { type: Boolean, required: true, default: false},
   lastUpdated: { type: Date, default: Date.now()},
 });
 
