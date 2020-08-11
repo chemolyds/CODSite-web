bcadac43f41d502329acc64d1773cdcd4f0a44c4
Jerdan1980 <jeremytinana@gmail.com>
2020-08-11 15:12:24 -0400

    Re-add LocalHost

--- client/src/components/ListFAQs.js
+++ client/src/components/ListFAQs.js
@@ -52,7 +52,7 @@ const ListFAQs = (props) => {
 	}
 
 	useEffect(() => {
-		axios.get(`/api/user/get_faq`) 
+		axios.get(`http://localhost:3001/api/user/get_faq`) 
 			.then(res => {setFAQs(res.data)});
 	}, []);
 
