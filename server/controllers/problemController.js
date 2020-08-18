1561db311b19a26304f1bb86118c5e1501993634
Jerdan1980 <jeremytinana@gmail.com>
2020-08-18 14:14:10 -0400

    Add Categories

--- server/controllers/problemController.js
+++ server/controllers/problemController.js
@@ -1,9 +1,46 @@
 import path from 'path';
 import Problem from '../models/problemModel.js';
+import Categories from '../models/categoriesModel.js';
 
 import {dirname} from 'path';
 import {fileURLToPath} from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export const createCategories = async (req, res) => {
+	let save_categories
+	save_categories = new Categories({
+		name: req.body.name,
+		categories: req.body.categories
+	});
+
+	save_categories.save(function (err, save_categories) {
+		if(err) {
+			return res.status(400).json(err);
+		} else {
+			console.log('saved =>', save_categories);
+			return res.status(200).json(save_categories);
+		}
+	})
+};
+
+export const getCategories = async (req, res) => {
+	Categories.findOne({ name: "problems"}, (err, data) => {
+		res.status(200).json(data);
+	});
+}
+
+export const editCategories = async (req, res) => {
+	Categories.findOneAndUpdate({ name: "problems" }, req.body, { new: true }, (err, data) => {
+		if (err) {
+			res.status(400).json(err);
+			throw err;
+		} else if (!data) {
+			res.status(400).json({ message: 'Page contents does not exist!'});
+		} else {
+			res.status(200).json(data);
+		}
+	});
+}
 
 export const getProblemList = async (req, res) => {
 	Problem.find({}, (err, data) => {
