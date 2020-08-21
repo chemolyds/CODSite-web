f138dd7cf0349ee6aa9b27fb5d55d6c6fd9392b6
Jerdan1980 <jeremytinana@gmail.com>
2020-08-21 14:45:22 -0400

    Fix typo

--- client/src/pages/Problems.js
+++ client/src/pages/Problems.js
@@ -6,7 +6,7 @@ import PageContents from '../components/PageContents';
 import ListProblems from '../components/ListProblems';
 import Footer from '../components/Footer';
 
-const Home = () => {
+const Problems = () => {
 	return (
 		<div className="App">
 			<NavBar page="Problems"/>
@@ -19,4 +19,4 @@ const Home = () => {
 	)
 }
 
-export default Home;
+export default Problems;
