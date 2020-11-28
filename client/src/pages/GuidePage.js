e13ce28647c65dd366fc6c1a0be6d0f169249e43
Jerdan1980 <jeremytinana@gmail.com>
2020-11-28 12:13:15 -0500

    Finish GuidePage

--- client/src/pages/GuidePage.js
+++ client/src/pages/GuidePage.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
+import jwt from "jsonwebtoken";
 import {Card, CardColumns} from 'react-bootstrap';
 import '../App.css';
 
@@ -10,6 +11,8 @@ import {ArrowLeftIcon, TriangleLeftIcon} from '@primer/octicons-react';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import CreateSubpage from '../components/Guides/CreateSubpage';
+import DeleteSubpage from '../components/Guides/DeleteSubpage';
+import EditSubpage from '../components/Guides/EditSubpage';
 
 //https://stackoverflow.com/questions/42463263/wrapping-a-react-router-link-in-an-html-button
 
@@ -26,15 +29,60 @@ var md = require("markdown-it")({html: true, linkify: true, typographer: true})
 	.use(require('markdown-it-sub'))
 	.use(require("markdown-it-sup"));
 
-const Guides = ({match, location}) => {
-	const [header, setHeader] = useState("");
+const GuidePage = ({match, location}) => {
+	const [guide, setGuide] = useState({});
 	const [description, setDescription] = useState("");
 	const [subpages, setSubpages] = useState([]);
+
+	const addable = () => {
+		const token = localStorage.getItem("user_logged");
+		let isAdmin;
+		if (token) {
+			jwt.verify(token, "jerdan1980", function (err, decoded) {
+				if (decoded) {
+					isAdmin = decoded.user_info.isAdmin;
+				}
+			});
+			if (isAdmin) {
+				return (
+					<Card>
+						<Card.Title class="h3 pt-4 mb-0 pb-0">Settings</Card.Title>
+						<Card.Body>
+							<div class="btn-group">
+								<CreateSubpage GuideID={match.params['guide']}/>
+							</div>
+						</Card.Body>
+					</Card>
+				)
+			}
+		}
+	}
+
+	const editable = (subpage) => {
+		const token = localStorage.getItem("user_logged");
+		let isAdmin;
+		if (token) {
+			jwt.verify(token, "jerdan1980", function (err, decoded) {
+				if (decoded) {
+					isAdmin = decoded.user_info.isAdmin;
+				}
+			});
+			if (isAdmin) {
+				return (
+					<div class="btn-group">
+						<EditSubpage guideID={match.params['guide']} subpage={subpage}/>
+						<DeleteSubpage guideID={match.params['guide']} subpage={subpage}/>
+					</div>
+				)
+			}
+		}
+	}
 
 	useEffect(() => {
 		axios.get(`http://localhost:3001/api/guide/get_guide/${match.params['guide']}`)
 			.then(res => {
-				setHeader(res.data.header);
+				setGuide(res.data);
+				//for some reason guide.description =/= description
 				setDescription(res.data.description);
 				setSubpages(res.data.subpages);
 			})
@@ -49,9 +97,10 @@ const Guides = ({match, location}) => {
 					<p>{subpage.header}</p>
 					<div class="d-flex justify-content-between align-items-center">
 						<div class="btn-group">
-							<Link to={`guides/${match.params['guide']}/${subpage._id}`}>
+							<Link to={`${match.params['guide']}/${subpage._id}`}>
 								<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
 							</Link>
+							{editable(subpage)}
 						</div>
 					</div>
 				</Card.Body>
@@ -74,7 +123,7 @@ const Guides = ({match, location}) => {
 					</Link>
 
 					<div class="col">
-						<h1 class="display-1">{header}</h1>
+						<h1 class="display-1">{guide.header}</h1>
 					</div>
 
 					<div class="col-3"/>
@@ -86,25 +135,13 @@ const Guides = ({match, location}) => {
 					</div>
 				</div>
 			</div>
-
+			
 			<div class="container">
-				<p class="py-1"/>
-
+				<p class="py-1"/>	
 				<CardColumns>
 					{SubpageList}
-					<Card>
-						<Card.Title class="h3 pt-4 mb-0 pb-0">Settings</Card.Title>
-						<Card.Body>
-							<div class="btn-group">
-								<CreateSubpage GuideID={match.params['guide']}/>
-								<button type="button" class="btn btn-sm btn-outline-secondary" /*onClick={handleShow}*/>
-									Reorder
-								</button>
-							</div>
-						</Card.Body>
-					</Card>
+					{addable()}
 				</CardColumns>
-
 			</div>
 			
 			<Footer/>
@@ -112,4 +149,4 @@ const Guides = ({match, location}) => {
 	)
 }
 
-export default Guides;
+export default GuidePage;
